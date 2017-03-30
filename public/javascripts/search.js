3b176efe24d895b6e68a1a3f3627781643fcda1f
kathicks <kmhicks92@gmail.com>
2017-03-30 17:10:38 +0100

    Add client-side javascript for dynamically hiding and revealing search bar on icon click

---
+++ public/javascripts/search.js
@@ -0,0 +1,31 @@
+var search = (function () {
+    var searchBar   = document.getElementById('search-box');
+    var searchIcon  = document.getElementById('search-icon');
+    var searchClose = document.getElementById('close-search');
+
+    var hide = function (element) {
+        element.style.display = 'none';
+    };
+
+    var show = function (element) {
+        element.style.display = 'block';
+    };
+
+    var init = function () {
+        searchIcon.addEventListener('click', function () {
+            show(searchBar);
+        });
+
+        searchClose.addEventListener('click', function () {
+            hide(searchBar);
+        });
+    };
+
+    return {
+        init: init
+    };
+}());
+
+window.onload = function () {
+  search.init();
+};
