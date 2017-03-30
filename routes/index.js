24602ca3a8d50268cfffa694b86e13bb08800b32
kathicks <kmhicks92@gmail.com>
2017-03-30 16:07:48 +0100

    Add routing logic for search term so that it can post and redirect to index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var request = require("request");
-var options = require("../data/api.js")
+var options = require("../data/api.js");
 
 var router = express.Router();
 
@@ -10,9 +10,27 @@ router.get('/', function (req, res) {
     var apiParams = options();
 
     request(apiParams, function (error, response, body) {
-      if (error) console.log(error);
-      
-      res.render('index', { results : body.results[0].results });
+        if (error) {
+            console.log(error);
+        };
+        res.render('index', { results : body.results[0].results });
+    });
+});
+
+router.post('/search', function (req, res) {
+    var searchTerm = req.body.search.toLowerCase();
+    res.redirect('/' + searchTerm);
+});
+
+router.get('/:term', function (req, res) {
+    var searchTerm = req.params.term;
+    var apiParams = options(searchTerm);
+
+    request(apiParams, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        };
+        res.render('index', { results : body.results[0].results });
     });
 });
 
