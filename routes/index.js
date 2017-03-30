1477209f3a662dbb02e58c2100e93487a84e90e3
kathicks <kmhicks92@gmail.com>
2017-03-30 11:17:17 +0100

    Add API call to index route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 'use strict';
 
 var express = require('express');
+var request = require("request");
+
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index');
+    var options = {
+      method: 'POST',
+      url: 'http://api.ft.com/content/search/v1',
+      headers: {
+        'x-api-key': process.env.FT_API_KEY,
+        'content-type': 'application/json'
+      },
+      body: {
+        queryString: '',
+        queryContext: {
+          curations: [ 'ARTICLES' ]
+        },
+        resultContext: {
+          aspects: [ 'title', 'summary', 'lifecycle' ]
+        }
+      },
+      json: true
+    };
+
+    request(options, function (error, response, body) {
+      if (error) console.log(error);
+
+      res.render('index', { results : body.results[0].results });
+    });
 });
 
 module.exports = router;
