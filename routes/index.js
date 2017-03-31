abc593e315086e9fcc50112a5694cc16e36bf20a
kathicks <kmhicks92@gmail.com>
2017-03-31 17:30:15 +0100

    Install request-promises and use to reconfigure api request to return paginated results

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
 'use strict';
 
-var express = require('express');
-var request = require("request");
-var options = require("../data/api.js");
+var express     = require('express');
+var request     = require('request');
+var promRequest = require('request-promise');
+var options     = require('../data/api.js');
 
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    var apiParams = options();
+    var pages = new Array();
 
-    request(apiParams, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        };
-        res.render('index', { results : body.results[0].results });
-    });
+    for (var i = 0; i < 100; i += 20) {
+      var count = 0;
+      promRequest(options(i.toString()))
+          .then(function (body) {
+              count += 1;
+              pages.push(body.results[0].results);
+              if (count === 5) {
+                res.render('index', { results : pages });
+              }
+          })
+          .catch(function (error) {
+              if (error) {
+                  console.log(error);
+              };
+          });
+    };
 });
 
 router.post('/search', function (req, res) {
@@ -24,14 +35,24 @@ router.post('/search', function (req, res) {
 
 router.get('/:term', function (req, res) {
     var searchTerm = req.params.term;
-    var apiParams = options(searchTerm);
+    var pages = new Array();
 
-    request(apiParams, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        };
-        res.render('index', { results : body.results[0].results });
-    });
+    for (var i = 0; i < 100; i += 20) {
+      var count = 0;
+      promRequest(options(i.toString(), searchTerm))
+          .then(function (body) {
+              count += 1;
+              pages.push(body.results[0].results);
+              if (count === 5) {
+                res.render('index', { results : pages });
+              }
+          })
+          .catch(function (error) {
+              if (error) {
+                  console.log(error);
+              };
+          });
+    };
 });
 
 module.exports = router;
